40040f5ff2911f9f45779d6889c4305fa9de320f
Zhengting02 <101455471+Zhengting02@users.noreply.github.com>
2022-03-15 00:00:31 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -21,8 +21,8 @@ import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', app.views.index, name='index'),
-    path('add', app.views.add, name='add'),
-    path('view/<str:id>', app.views.view, name='view'),
-    path('edit/<str:id>', app.views.edit, name='edit'),
+    path('', app.views.index, name='login'),
+    #path('add', app.views.add, name='add'),
+    #path('view/<str:id>', app.views.view, name='view'),
+    #path('edit/<str:id>', app.views.edit, name='edit'),
 ]
