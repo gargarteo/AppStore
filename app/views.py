a592e1c9f286a208325ae3ae33a876d54a1ae1b1
Zhengting02 <101455471+Zhengting02@users.noreply.github.com>
2022-03-15 01:34:24 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ def index(request):
     if request.POST:
         if request.POST['action'] == 'delete':
             with connection.cursor() as cursor:
-                cursor.execute("DELETE FROM customers WHERE customerid = %s", [request.POST['id']])
+                cursor.execute("DELETE FROM users WHERE customerid = %s", [request.POST['school_email']])
 
     ## Use raw query to get all objects
     with connection.cursor() as cursor:
