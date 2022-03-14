79a4442b7d635e0d5902e3efa636d141e6b0e367
Zhengting02 <101455471+Zhengting02@users.noreply.github.com>
2022-03-15 00:23:04 +0800

    Update AppStoreSchema Original.sql

--- sql/AppStoreSchema Original.sql
+++ sql/AppStoreSchema Original.sql
@@ -4,24 +4,53 @@
 
 ********************/
 
-CREATE TABLE IF NOT EXISTS customers (
- first_name VARCHAR(64) NOT NULL,
- last_name VARCHAR(64) NOT NULL,
- email VARCHAR(64) UNIQUE NOT NULL,
- dob DATE NOT NULL,
- since DATE NOT NULL,
- customerid VARCHAR(16) PRIMARY KEY,
- country VARCHAR(16) NOT NULL);
-	
-CREATE TABLE IF NOT EXISTS games(
- name VARCHAR(32),
- version CHAR(3),
- price NUMERIC NOT NULL,
- PRIMARY KEY (name, version));
-  
- CREATE TABLE downloads(
- customerid VARCHAR(16) REFERENCES customers(customerid) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
- name VARCHAR(32),
- version CHAR(3),
- PRIMARY KEY (customerid, name, version),
- FOREIGN KEY (name, version) REFERENCES games(name, version) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED);
+CREATE TABLE users (
+name VARCHAR(64) NOT NULL, 
+school_email VARCHAR(64) PRIMARY KEY CHECK (school_email='%@u.nus.edu'), 
+current_borrowed_items NUMERIC CHECK(current_borrowed_items<= 2) NOT NULL DEFAULT 0.00,
+demerit_points NUMERIC NOT NULL CHECK(demerit_points<= 8) DEFAULT 0.00,
+vouchers_points NUMERIC NOT NULL DEFAULT 0.00,
+max_request NUMERIC NOT NULL DEFAULT 2.00,
+password VARCHAR(64) NOT NULL,
+CHECK (users.current_borrowed_items <= users.max_request)
+CHECK (len(users.password) >= 6)
+);
+
+CREATE TABLE category (
+category VARCHAR(64) PRIMARY KEY UNIQUE
+);
+
+CREATE TABLE location (
+location VARCHAR(64) PRIMARY KEY UNIQUE
+);
+
+CREATE TABLE request (
+request_id NUMERIC PRIMARY KEY,
+item VARCHAR(64) NOT NULL,
+loaner VARCHAR(64) REFERENCES users(school_email) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+category VARCHAR(64) REFERENCES category(category) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+date_needed DATE NOT NULL,
+time_needed TIME NOT NULL,
+return TIMESTAMP NOT NULL,
+meetup_location VARCHAR(64) REFERENCES location(location) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED
+);
+
+CREATE TABLE loan (
+transaction_id NUMERIC PRIMARY KEY REFERENCES request(request_id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+borrower VARCHAR(64) NOT NULL REFERENCES users(school_email),
+owner VARCHAR(64) NOT NULL REFERENCES users(school_email),
+item VARCHAR(64) NOT NULL,
+date_borrowed DATE NOT NULL,
+return_deadline DATE,
+returned_date DATE
+);
+
+
+CREATE TABLE vouchers(
+voucher_id VARCHAR(64) PRIMARY KEY,
+voucher_name VARCHAR(64) NOT NULL,
+merchant_name VARCHAR(64) NOT NULL,
+voucher_value INTEGER NOT NULL,
+points_required NUMERIC,
+used BOOLEAN NOT NULL DEFAULT FALSE,
+owner_of_voucher VARCHAR(64) references users(school_email));
