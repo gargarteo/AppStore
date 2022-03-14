65a6eea064c8c01ee058b38d8fd68cdc9946af3c
Zhengting02 <101455471+Zhengting02@users.noreply.github.com>
2022-03-15 01:27:27 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.db import connection
-
+'''
 # Create your views here.
-'''def index(request):
+def index(request):
     """Shows the main page"""
 
     ## Delete customer
