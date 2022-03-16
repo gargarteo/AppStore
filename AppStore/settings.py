e9f147ba81e3d4832b8c4d002271dcac36a32e87
phoebetyr <98776880+phoebetyr@users.noreply.github.com>
2022-03-17 01:42:38 +0800

    Update settings.py

--- AppStore/settings.py
+++ AppStore/settings.py
@@ -144,8 +144,6 @@ USE_TZ = True
 
 
 
-
-SESSION_ENGINE ="django.contrib.sessions.backends.signed_cookies"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
