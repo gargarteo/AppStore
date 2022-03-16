b8895dcbec0b1a644d4eb185fc2a9e487e6ec103
gargarteo <101463773+gargarteo@users.noreply.github.com>
2022-03-16 15:21:11 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('register', app.views.create_account, name='register'),
     path('add', app.views.add, name='add'),
     path('view/<str:id>', app.views.view, name='view'),
-    path('edit/<str:id>', app.views.edit, name='edit')
+    path('edit/<str:id>', app.views.edit, name='edit'),
     path('home', app.views.home, name = 'home')
 ]
