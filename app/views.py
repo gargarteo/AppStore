8e55124f8b58228ec32126ee788ba7432d4498eb
Zhengting02 <101455471+Zhengting02@users.noreply.github.com>
2022-03-16 20:41:44 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -28,7 +28,7 @@ def index(request):
         if account == None:
             status = 'Wrong Login Details'
         else:
-            return render(request, "app/home.html". {'email' : email}
+            return render(request, "app/home.html", {'email' : email}
         
         context['status'] = status
         return render(request, "app/index.html", context)
