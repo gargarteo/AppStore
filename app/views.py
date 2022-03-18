a012f01d6d44906f06995c310c3da5effb30cc79
phoebetyr <98776880+phoebetyr@users.noreply.github.com>
2022-03-18 21:53:49 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.db import connection 
 from django.http import HttpResponse  
-manage.py syncdb
+
 def new_request(request):
     context = {}
     status = ''
