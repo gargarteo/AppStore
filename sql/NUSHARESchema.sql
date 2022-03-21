3297f3e537919b0b1201245cb81d3fac056dcb69
gargarteo <101463773+gargarteo@users.noreply.github.com>
2022-03-21 20:48:17 +0800

    Update NUSHARESchema.sql

--- sql/NUSHARESchema.sql
+++ sql/NUSHARESchema.sql
@@ -12,6 +12,7 @@ demerit_points NUMERIC NOT NULL CHECK(demerit_points<= 8) DEFAULT 0.00,
 vouchers_points NUMERIC NOT NULL DEFAULT 0.00,
 max_request NUMERIC NOT NULL DEFAULT 2.00,
 password VARCHAR(64) NOT NULL,
+suspend BOOLEAN,
 CHECK (users.current_borrowed_items <= users.max_request),
 CHECK (LENGTH(users.password) >= 6)
 );
