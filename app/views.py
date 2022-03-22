1cb91f41276781f2b405f1c3b157c447173829f4
kunmingang <70063579+kunmingang@users.noreply.github.com>
2022-03-23 02:48:07 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -117,16 +117,14 @@ def home(request):
                cursor.execute("SELECT return_date FROM requests WHERE request_id=%s",[request.POST['id']])
                return_deadline= (cursor.fetchone())
                returned_date= return_deadline
-               if borrower==request.session['email']:
+               if borrower<>request.session['email']:
+                    cursor.execute("INSERT INTO loan VALUES (%s, %s, %s, %s, %s, %s, %s)", [request.POST['id'], borrower, request.session['email'], item , date_borrowed, return_deadline, returned_date])
+                    cursor.execute("UPDATE requests SET accepted=true WHERE request_id=%s",[request.POST['id']])
+                    return redirect('profile')
+               else:
                     status = 'You cannot accept your own requests!'
                     context['status'] = status
                     return render(request,'app/home.html',context)
-               else:
-                    cursor.execute("INSERT INTO loan VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                            , [request.POST['id'], borrower, request.session['email'],
-                               item , date_borrowed, return_deadline, returned_date])
-                    cursor.execute("UPDATE requests SET accepted=true WHERE request_id=%s",[request.POST['id']])
-                    return redirect('profile')
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM requests WHERE accepted=false ORDER BY date_needed ASC")
         requests = cursor.fetchall()           
