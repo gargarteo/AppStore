47ae8a8a0e43ef1e6e6aebfba97a4c3f1fc638c0
gargarteo <101463773+gargarteo@users.noreply.github.com>
2022-03-31 16:04:30 +0800

    Update NUSHAREvouch.sql

--- sql/NUSHAREvouch.sql
+++ sql/NUSHAREvouch.sql
@@ -1,3 +1,3 @@
-INSERT INTO vouch VALUES('Gong Cha $5', 'Gong Cha', 5, 500.00)
-INSERT INTO vouch VALUES('Koi $5', 'Koi', 5, 500) 
-INSERT INTO vouch VALUES('Hwangs Free Gyoza', 'Hwangs', 4, 400) 
+INSERT INTO vouch VALUES('Gong Cha $5', 'Gong Cha', 5, 500.00);
+INSERT INTO vouch VALUES('Koi $5', 'Koi', 5, 500);
+INSERT INTO vouch VALUES('Hwangs Free Gyoza', 'Hwangs', 4, 400); 
