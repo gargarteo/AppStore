45bbc3c7a5e08606e6dfc357eb7d62f59197c8e3
phoebetyr <98776880+phoebetyr@users.noreply.github.com>
2022-04-02 14:42:07 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -265,7 +265,7 @@ def index(request):
             cursor.execute("SELECT school_email, password FROM users WHERE school_email = %s AND password = %s AND suspend = FALSE", [request.POST['school_email'],request.POST['password']])
             account = cursor.fetchone()
             cursor.execute("UPDATE loan SET days_overdue= (CURRENT_DATE- return_deadline) WHERE return_deadline<CURRENT_DATE")
-            cursor.execute("CREATE VIEW tempp AS SELECT borrower, COALESCE(SUM(days_overdue),0) as sum FROM loan GROUP BY borrower")
+            cursor.execute("CREATE OR REPLACE VIEW tempp AS SELECT borrower, COALESCE(SUM(days_overdue),0) as sum FROM loan GROUP BY borrower")
             cursor.execute("UPDATE users SET demerit_points= t.sum FROM tempp t, users u WHERE t.borrower=u.school_email")
             if account:
                 cursor.execute("SELECT COALESCE(SUM(days_overdue),0) FROM loan WHERE borrower=%s", [request.POST['school_email']])
